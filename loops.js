bcd2f93e1c68a80ad5f01a7fe849d04c13e8bdff
Min Kang <kangm914@gmail.com>
2019-04-25 17:31:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,3 +28,14 @@ function doWhileLoop(num){
     console.log("I run once regardless.");
   } while (incrementVariable() < num); 
 } 
+
+function doWhileLoop(num){
+  var i=0;
+
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {console.log("I run once regardless.")} while(incrementVariable() < num)
+} 
